c021f2eaa851efffe34d4068fc2f6e636798da90
Mithun <mithuncm@gmail.com>
2024-08-03 18:23:22 +0530

    LLD3: Machine Coding 12: Audit Annotations added on the BaseModel

--- src/main/java/uber/kautilya/machinecoding/models/BaseModel.java
+++ src/main/java/uber/kautilya/machinecoding/models/BaseModel.java
@@ -1,19 +1,25 @@
 package uber.kautilya.machinecoding.models;
 
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
 
 @Getter
 @Setter
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass //Keep all attributes in all subclasses, while maintaining them private
 public class BaseModel {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @CreatedDate
     private Date creationDate;
+    @LastModifiedDate
     private Date modificationDate;
 }
 /*
